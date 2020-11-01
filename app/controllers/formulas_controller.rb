9ebe9d3084fe8354c36f51be8c1330587780de2d
zacharytq <zacharytq@gmail.com>
2020-11-01 12:21:01 -0500

    formula new and create and show

--- app/controllers/formulas_controller.rb
+++ app/controllers/formulas_controller.rb
@@ -1,12 +1,28 @@
-class FormulaController < ApplicationController
+class FormulasController < ApplicationController
 
   def new
+    @formula = Formula.new
   end
 
   def create
+    if logged_in?
+      @formula = Formula.new(formula_params)
+      if @formula.save
+        redirect_to formula_path(@formula)
+      else
+        render :new
+      end
+    else
+      redirect_to root_path
+    end
   end
 
   def show
+    if logged_in?
+      @formula = Formula.find(params[:id])
+    else
+      redirect_to root_path
+    end
   end
 
   private
